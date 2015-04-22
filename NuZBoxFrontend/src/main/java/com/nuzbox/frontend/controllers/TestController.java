16a2900904b81ff3b6ea18610260bbac55149b03
Farrell Hayman <farrell@bluetoaster.net>
2015-04-22 17:08:55 +0100

    logging, bitch

--- NuZBoxFrontend/src/main/java/com/nuzbox/frontend/controllers/TestController.java
+++ NuZBoxFrontend/src/main/java/com/nuzbox/frontend/controllers/TestController.java
@@ -1,9 +1,12 @@
 package com.nuzbox.frontend.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.logging.Logger;
 
 /**
  * Created by Jamesjohnstone on 22/04/15.
@@ -11,8 +14,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class TestController {
 
+    static Logger log = Logger.getLogger(TestController.class.getName());
     @RequestMapping(value = "/index", method = RequestMethod.GET)
-    public String printHello(ModelMap model) {
+    public String printHello(Model model) {
+        log.info("I am being run!");
         model.addAttribute("message", "Hello Spring MVC Framework!");
         return "index";
     }
