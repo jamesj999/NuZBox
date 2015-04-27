12abf8c4da09ea7e4cdf4165b2700d5dc777fbe2
Jamesj999 <jamesj999@gmail.com>
2015-04-27 18:03:49 +0100

    Autowiring now works in nuzbox-tasks (albeit via a workaround for now).

--- nuzbox-tasks/src/main/java/com/nuzbox/tasks/TasksStartup.java
+++ nuzbox-tasks/src/main/java/com/nuzbox/tasks/TasksStartup.java
@@ -1,31 +1,38 @@
 package com.nuzbox.tasks;
 
-import com.nuzbox.model.CronJobModel;
 import com.nuzbox.model.service.ModelService;
-import com.nuzbox.tasks.service.CronJobService;
 import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
 
 
 import javax.servlet.*;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Enumeration;
 
 /**
  * Created by james on 24/04/15.
  */
 public class TasksStartup implements Servlet {
     private static final Logger LOG = Logger.getLogger(TasksStartup.class);
-    @Autowired
-    private ModelService modelService;
+    private ModelService modelService = (ModelService) ApplicationContextProvider.getApplicationContext().getBean("modelService");
 
     public void init(ServletConfig servletConfig) throws ServletException {
-        if(modelService == null) {
-            LOG.error("PISS");
-        } else {
-            LOG.error("SHIT");
+        LOG.warn("ModelService: "+modelService);
+
+        String parameterNames = "";
+        Enumeration<String> paramNames = servletConfig.getInitParameterNames();
+        while(paramNames.hasMoreElements()) {
+            parameterNames = parameterNames + ", " + paramNames.nextElement();
         }
+
+        String attributeNames = "";
+        Enumeration<String> attNames = servletConfig.getServletContext().getAttributeNames();
+        while(attNames.hasMoreElements()) {
+            attributeNames = attributeNames + ", " + attNames.nextElement();
+        }
+
+        LOG.info(parameterNames);
+        LOG.info(attributeNames);
+        LOG.info(servletConfig.getServletName());
     }
 
     public ServletConfig getServletConfig() {
@@ -41,6 +48,5 @@ public class TasksStartup implements Servlet {
     }
 
     public void destroy() {
-
     }
 }
