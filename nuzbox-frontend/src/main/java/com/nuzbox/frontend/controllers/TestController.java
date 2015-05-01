f5a400b87ba30513cc63bda58ca99129c6bd2af3
Farrell Hayman <farrell@bluetoaster.net>
2015-05-01 11:43:51 +0100

    Article fun

--- nuzbox-frontend/src/main/java/com/nuzbox/frontend/controllers/TestController.java
+++ nuzbox-frontend/src/main/java/com/nuzbox/frontend/controllers/TestController.java
@@ -9,7 +9,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import org.apache.commons.net.PrintCommandListener;
+import org.apache.commons.net.nntp.NNTPClient;
+import org.apache.commons.net.nntp.NewsgroupInfo;
+
+
 import org.apache.log4j.Logger;
+
+import java.io.BufferedReader;
+import java.io.PrintWriter;
 
 /**
  * Created by Jamesjohnstone on 22/04/15.
@@ -24,7 +32,40 @@ public class TestController {
     @RequestMapping(value = {"/", "/index**"}, method = RequestMethod.GET)
     public String printHello(ModelMap model) {
         LOG.info("I am being run!");
-        modelService.save(null);
+
+        String hostname = "x";
+        String username = "y";
+        String password = "z";
+
+        try {
+            NNTPClient client = new NNTPClient();
+            client.addProtocolCommandListener(new PrintCommandListener(new PrintWriter(System.out), true));
+            client.connect(hostname);
+            client.authenticate(username, password);
+
+
+            NewsgroupInfo group = new NewsgroupInfo();
+            client.selectNewsgroup("alt.binaries.hdtv.x264",group);
+
+            BufferedReader br;
+            String line;
+            long articleNum = group.getLastArticleLong();
+            br = client.retrieveArticleHeader(articleNum);
+
+            StringBuffer buffer = new StringBuffer();
+            if (br != null) {
+                while ((line = br.readLine()) != null) {
+                    LOG.info(line);
+                    buffer.append(line);
+                }
+                model.addAttribute("article",buffer.toString());
+            }
+            br.close();
+
+        }
+        catch (Exception ignore ){}
+
+
         String test = "Test";
         model.addAttribute("testMessage", test);
         return "index";
